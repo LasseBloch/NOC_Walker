e7695379cb7b2c1af168cab41f2cbae44ff450fd
LasseBloch <lassebloch@gmail.com>
2017-02-04 11:52:57 +0100

    simplified walk

--- src/walker.cpp
+++ src/walker.cpp
@@ -6,8 +6,8 @@
 walker::walker(int screenWidth, int screenHeight) : startXPos_(screenWidth / 2), startYPos_(screenHeight / 2), screenHeight_ (screenHeight), screenWidth_(screenWidth), rnd_(time(0))  {
   xPos_ = startXPos_;
   yPos_ = startYPos_;
-  gen_ = std::uniform_int_distribution<int>(0,3);
   speed_ = 1;
+  gen_ = std::uniform_int_distribution<int>(-speed_,speed_);
 }
 
 std::pair<int, int> walker::getPos() const {
@@ -16,20 +16,8 @@ std::pair<int, int> walker::getPos() const {
 
 bool walker::walk() {
 
-  int n = gen_(rnd_);
-  if (n == 0) {
-    xPos_ -= speed_;
-  }
-  else if (n == 1) {
-    xPos_ += speed_;
-  }
-  else if (n == 2) {
-    yPos_ -= speed_;
-  }
-  else if (n == 3)
-  {
-    yPos_ += speed_;
-  }
+  xPos_ += gen_(rnd_);
+  yPos_ += gen_(rnd_);
 
   if (xPos_ <= screenWidth_ || xPos_ <= 0 || yPos_ <= screenHeight_ || yPos_ <= 0) {
     return true;
